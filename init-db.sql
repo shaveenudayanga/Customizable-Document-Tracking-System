c62356724d21671d2f9a2abaf84c1949c4832815
shaveenudayanga <shaveenudayanga@gmail.com>
2025-10-10 11:52:34 +0530

    chore: Remove outdated GitHub workflows and update Docker configurations for improved CI/CD pipeline

--- init-db.sql
+++ init-db.sql
@@ -1,14 +1,16 @@
--- Initialize databases for all services
-CREATE DATABASE IF NOT EXISTS user_db;
-CREATE DATABASE IF NOT EXISTS document_db;
-CREATE DATABASE IF NOT EXISTS workflow_db;
-CREATE DATABASE IF NOT EXISTS tracking_db;
-CREATE DATABASE IF NOT EXISTS notification_db;
+SELECT 'CREATE DATABASE user_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'user_db')\gexec
+SELECT 'CREATE DATABASE document_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'document_db')\gexec
+SELECT 'CREATE DATABASE workflow_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'workflow_db')\gexec
+SELECT 'CREATE DATABASE tracking_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tracking_db')\gexec
+SELECT 'CREATE DATABASE notification_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'notification_db')\gexec
 
--- Create user with permissions
-CREATE USER IF NOT EXISTS docutrace WITH PASSWORD 'docutrace123';
-GRANT ALL PRIVILEGES ON DATABASE user_db TO docutrace;
-GRANT ALL PRIVILEGES ON DATABASE document_db TO docutrace;
-GRANT ALL PRIVILEGES ON DATABASE workflow_db TO docutrace;
-GRANT ALL PRIVILEGES ON DATABASE tracking_db TO docutrace;
-GRANT ALL PRIVILEGES ON DATABASE notification_db TO docutrace;
+GRANT ALL PRIVILEGES ON DATABASE user_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE document_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE workflow_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE tracking_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE notification_db TO postgres;
